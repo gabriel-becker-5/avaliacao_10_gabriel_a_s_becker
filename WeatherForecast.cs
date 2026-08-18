fbc491d60b334df5e553fc5da7a42fb3a36806a2
gabriel-becker-5 <gabriel.alexandre.becker@gmail.com>
2026-08-18 09:41:20 -0300

    Adicionar arquivos de projeto.

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace avaliacao_10_gabriel_a_s_becker
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
